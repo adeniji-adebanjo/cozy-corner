dd000cf2079a11d2cdaecad9be675551f5ce53a9
adeniji-adebanjo <aeben.adebanjo@gmail.com>
2024-09-11 16:13:36 +0100

    adjusted the codes for better performance and responsiveness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+
 function App() {
   return (
     <div className="App">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,40 +2,72 @@ import React from "react";
 import "../assets/css/Hero.css";
 import heroImage from "../assets/images/hero-img.png";
 import arrowimage from "../assets/images/arrow.svg";
+import location from "../assets/svg/Location.svg";
+import room from "../assets/svg/Room.svg";
+import wifi from "../assets/svg/wifi.svg";
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-image-container">
-        <img src={heroImage} alt="Hero" className="hero-image" />
-        <img src={arrowimage} alt="arrow" className="arrow-image" />
-      </div>
-      <div className="hero-content-container">
-        <div className="hero-content">
-          <h1>
-            Find your comfort <br /> working zone with <br /> Cozy Corner
-          </h1>
-          <h6>
-            Change your work environment by using a co-working place, <br />
-            to meet new people and gain a new network
-          </h6>
-          <div className="checkboxes">
-            <div className="checkbox-group">
-              <input type="checkbox" id="checkbox1" />
-              <label htmlFor="checkbox1">Option 1</label>
-              <input type="checkbox" id="checkbox2" />
-              <label htmlFor="checkbox2">Option 2</label>
-            </div>
-            <div className="checkbox-group">
-              <input type="checkbox" id="checkbox3" />
-              <label htmlFor="checkbox3">Option 3</label>
-              <input type="checkbox" id="checkbox4" />
-              <label htmlFor="checkbox4">Option 4</label>
+    <>
+      <section>
+        <div className="hero-section">
+          <div className=" hero-image-container">
+            <img src={heroImage} alt="Hero" className="hero-image" />
+          </div>
+          <div className="hero-content-container">
+            <div className="hero-content">
+              <h1>
+                Find your comfort <br /> working zone with <br /> Cozy Corner
+              </h1>
+              <h6>
+                Change your work environment by using a co-working place, <br />
+                to meet new people and gain a new network
+              </h6>
             </div>
           </div>
         </div>
+
+        <div className="slider">
+          <div className="slider1">
+            <img src={arrowimage} alt="arrow" className="arrow-image" />
+          </div>
+          <div className="slider2">
+            <div className="cozy-options">
+              <div className="opt">
+                <ul>
+                  <li>Virtual Office Event</li>
+                  <li>Space for Office </li>
+                </ul>
+              </div>
+              <div className="opt">
+                <ul>
+                  <li>Open Workspace </li>
+                  <li>Chill-out Zone</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="perks">
+        <div className="perk1">
+          <img src={location} height={"80px"} width={"80px"} alt="" />
+          <h4>20+ location</h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="perk2">
+          <img src={room} height={"80px"} width={"80px"} alt="" />
+          <h4>324 Rooms Available</h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="perk3">
+          <img src={wifi} height={"80px"} width={"80px"} alt="" />
+          <h4>100% Internet Connection</h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
       </div>
-    </section>
+    </>
   );
 };
 
